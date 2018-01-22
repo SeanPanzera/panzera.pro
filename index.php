3485a528202c1d3db285e01a5dfef23bade9a5e1
zackbrand <zackbrand@gmail.com>
2018-01-22 15:59:19 -0500

    Added project structure for with support for Sass and Babel using Node

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"> 
+    <title>Test</title>
+    <style><?php include('min.css'); ?></style>
+    <script><?php include('min.js'); ?></script>
+  </head>
+  <body>
+    <div id="wrap">
+      <h1>Test</h1>
+    </div>
+  </body>
+</html>
---
+++ src/lib/custom.js
@@ -0,0 +1,3 @@
+let text = "ES6 was here.";
+
+console.log(text);
