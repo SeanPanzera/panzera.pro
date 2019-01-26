64f69901fa004dc9647cf00292a65dcef81a0553
seanpanzera <s.panzera@gmail.com>
2019-01-26 16:11:47 -0500

    Made everything more colorful

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="home">
   <head>
     <meta charset="utf-8"> 
     <title>Test</title>
@@ -16,6 +16,22 @@
     --> 
   </style>
   </head>
+  <navigation class="o-navigation">
+    <ul class="c-nav-list">
+      <li class="c-nav-list__item c-nav-list__home">
+        <a href="" class="cyan">Home</a>
+      </li>
+      <li class="c-nav-list__item">
+        <a href="" class="green">About</a>
+      </li>
+      <li class="c-nav-list__item">
+        <a href="" class="magenta">What's New</a>
+      </li>
+      <li class="c-nav-list__item">
+        <a href="" class="yellow">Why are you here?</a>
+      </li>
+    </ul>
+  </navigation>
   <body>
     <div class="o-wrap">
       <h1 class="c-greeting">Welcome.</h1>
