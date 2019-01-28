7bc715f73569383b9c759c8007b5c4b56c01ab9d
seanpanzera <s.panzera@gmail.com>
2019-01-28 14:12:25 -0500

    Added menu button and menu

--- src/lib/custom.js
+++ src/lib/custom.js
@@ -1,3 +1,16 @@
 let text = "If you're reading this, I've edited successfully.";
 
 console.log(text);
+
+$(function() {
+	$("svg").click(function() {
+		$(this).addClass("wobble");
+	});
+});
+
+$(function() {
+	$(".c-menu-button").click(function() {
+		$(".c-nav-list-mobile").toggleClass("c-nav-list-mobile__open");
+		$(this).toggleClass("c-menu-button__open");
+	});
+});
